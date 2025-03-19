d3ffff022ba9e0f36e93afd6c35618fd53df2b91
rgottberg <rgottberg@gmail.com>
2025-03-19 18:01:19 +0100

    recover first draft

---
+++ app.R
@@ -0,0 +1,106 @@
+# load clean data
+data <- rio::import("data/cleaned_data.csv")
+
+# Define UI ---------------------------------------------------------------
+ui <- 
+    page_fluid(
+        # app title ----
+        titlePanel("Business-Oriented Dashboard"),
+        # theme
+        theme = bslib::bs_theme(
+            bootswatch = "flatly",
+            version = "5"),
+        # sidebar layout with input and output definitions ----
+        navset_card_pill(
+            sidebar = sidebar(),
+            nav_panel(
+                title = "Visualizations",
+                layout_columns(
+                    col_widths = c(6,6),
+                    card(card_header("Plotly - Sales Trends"),
+                         full_screen = T,
+                         card_body(plotlyOutput("plotly"))
+                    )
+                    # card(card_header("Highcharter - Top Products"),
+                    #      full_screen = T,
+                    #      card_body(highchartOutput("highchart"))
+                    #      ),
+                    # card(card_header("Leaflet - Sales by Country"),
+                    #      full_screen = T,
+                    #      card_body(leafletOutput("leaflet"))
+                    #      ),
+                )
+            ),
+            nav_panel(
+                title = "DT",
+                layout_columns(
+                    col_widths = c(12),
+                    card(
+                        card_header("DT - Interactive Transaction Table"),
+                        full_screen = T,
+                        card_body(dataTableOutput("dt"))
+                    )
+                )
+            ),
+            nav_panel(
+                title = "Reactable",
+                layout_columns(
+                    col_widths = c(12),
+                    card(card_header("Reactable - Customer Insights"),
+                         full_screen = T,
+                         card_body(reactableOutput("react"))
+                    )
+                )
+            )
+        )
+    )
+
+# Define server logic -----------------------------------------------------
+server <- function(input, output) {
+    # data_filtered <- reactive({
+    #     data |>
+    #         dplyr::filter(Year == input$year) |>
+    #         dplyr::filter(Symbol == input$ticker)
+    # })
+    
+    output$plotly <- renderPlotly({
+        data |>
+            mutate(month = lubridate::month(InvoiceDate)) |>
+            group_by(month) |>
+            summarize(total_revenue = sum(Revenue)) |>
+            plot_ly(x = ~month,
+                    y = ~total_revenue,
+                    type="scatter",
+                    mode="lines") |>
+            layout(title = "Revenue by InvoiceDate")
+    })
+    
+    
+    
+    # output$highchart <- renderHighchart({
+    #     
+    # })
+    
+    # output$leaflet <- renderLeaflet({
+    #     # browser()
+    #     data2 |>
+    #         select(Country,Revenue)
+    #         # summarize()
+    # })
+    
+    output$dt <- renderDataTable({
+        data |>
+            DT::datatable(class = 'cell-border stripe',
+                          rownames = FALSE,
+                          filter = 'top',
+                          options = list(pageLength = 5))
+    })
+    
+    output$react <- renderReactable({
+        data |>
+            reactable::reactable()
+    })
+}
+# Run the application -----------------------------------------------------
+#shinyApp(ui, server, options = list(display.mode = "showcase"))
+shinyApp(ui, server, options = list())
